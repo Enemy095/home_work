a930e36831d28aa8a119bccf64e551454a6e3a19
Enemy095 <salam.tahaev@mail.ru>
2025-04-19 11:56:33 +0300

    lesson3

---
+++ src/main/java/org/work/lesson3/part3/fruit/Apple.java
@@ -0,0 +1,9 @@
+package org.work.lesson3.part3.fruit;
+
+public class Apple extends Fruits {
+
+    public Apple() {
+        name = "Apple";
+        weight = 1.0;
+    }
+}
---
+++ src/main/java/org/work/lesson3/part3/fruit/Orange.java
@@ -0,0 +1,8 @@
+package org.work.lesson3.part3.fruit;
+
+public class Orange extends Fruits {
+    public Orange() {
+        name = "Orange";
+        weight = 1.5;
+    }
+}
